ca16c624c41466fb0eb13e88bcbf63d8f762d5ea
tuxinghuan <m18817296539@163.com>
2019-01-13 13:44:08 +0800

    feat:增加更改颜色的api以及实现大小变换

--- Loading.vue
+++ Loading.vue
@@ -1,16 +1,55 @@
 <template>
     <div id='loading'>
-         <button v-on:click="show">
-            Toggle
-        </button>
         <div id='picture'>
-            <transition name ="fade"><div id='center' v-show="show1"><div class="Hexagon" :style=hexagonStyle></div></div></transition>
-            <transition name ="fade"><div id='top-left' v-show="show2"><div class="Hexagon" :style=hexagonStyle></div></div></transition>
-            <transition name ="fade"><div id='top-right' v-show="show3"><div class="Hexagon" :style=hexagonStyle></div></div></transition>
-            <transition name ="fade"><div id='right' v-show="show4"><div class="Hexagon" :style=hexagonStyle></div></div></transition>
-            <transition name ="fade"><div id='bottom-right' v-show="show5"><div class="Hexagon" :style=hexagonStyle></div></div></transition>
-            <transition name ="fade"><div id='bottom-left' v-show="show6"><div class="Hexagon" :style=hexagonStyle></div></div></transition>
-            <transition name ="fade"><div id='left' v-if="show7"><div class="Hexagon" :style=hexagonStyle></div></div></transition>
+            <transition name ="fade">
+                <div id='center' v-show="show1">
+                    <div class="beforeHexagon" :style=beforeHexagonStyle></div>
+                    <div class="Hexagon" :style=hexagonStyle></div>
+                    <div class="afterHexagon" :style=afterHexagonStyle></div>
+                </div>
+            </transition>
+            <transition name ="fade">
+            <div id='top-left' v-show="show2">
+                    <div class="beforeHexagon" :style=beforeHexagonStyle></div>
+                    <div class="Hexagon" :style=hexagonStyle></div>
+                    <div class="afterHexagon" :style=afterHexagonStyle></div>
+                </div>
+            </transition>
+            <transition name ="fade">
+            <div id='top-right' v-show="show3">
+                    <div class="beforeHexagon" :style=beforeHexagonStyle></div>
+                    <div class="Hexagon" :style=hexagonStyle></div>
+                    <div class="afterHexagon" :style=afterHexagonStyle></div>
+                </div>
+            </transition>
+            <transition name ="fade">
+            <div id='right' v-show="show4">
+                    <div class="beforeHexagon" :style=beforeHexagonStyle></div>
+                    <div class="Hexagon" :style=hexagonStyle></div>
+                    <div class="afterHexagon" :style=afterHexagonStyle></div>
+                </div>
+            </transition>
+            <transition name ="fade">
+            <div id='bottom-right' v-show="show5">
+                    <div class="beforeHexagon" :style=beforeHexagonStyle></div>
+                    <div class="Hexagon" :style=hexagonStyle></div>
+                    <div class="afterHexagon" :style=afterHexagonStyle></div>
+                </div>
+            </transition>
+            <transition name ="fade">
+            <div id='bottom-left' v-show="show6">
+                    <div class="beforeHexagon" :style=beforeHexagonStyle></div>
+                    <div class="Hexagon" :style=hexagonStyle></div>
+                    <div class="afterHexagon" :style=afterHexagonStyle></div>
+                </div>
+            </transition>
+            <transition name ="fade">
+            <div id='left' v-show="show7">
+                    <div class="beforeHexagon" :style=beforeHexagonStyle></div>
+                    <div class="Hexagon" :style=hexagonStyle></div>
+                    <div class="afterHexagon" :style=afterHexagonStyle></div>
+            </div>
+            </transition>
         </div>
     </div>
 </template>
@@ -18,6 +57,9 @@
 <script>
 export default {
     name:'loading',
+    props:{
+        color:String,
+    },
     data: function(){
         return{
             show1:true,
@@ -27,7 +69,6 @@ export default {
             show5:true,
             show6:true,
             show7:true,
-            color:'black',
             radius: '60px',
             gap: '10px'
         }
@@ -38,57 +79,69 @@ export default {
                 position: 'relative',
 	            width: '200px',
 	            height: '115px',
-                backgroundColor:'rgb(255, 208, 0)'
+                backgroundColor:this.color,
+                
+            }
+        },
+        beforeHexagonStyle:function(){
+            return{
+                borderTopColor:this.color
+            }
+        },
+        afterHexagonStyle:function(){
+            return{
+                borderBottomColor:this.color
             }
         }
     },
-    methods:{
-        show:function(){
+    mounted:function(){
             setInterval(() => {
                 setTimeout(() => {
-                    this.show1=!this.show1;
-                }, 200);
-                setTimeout(() => {
                     this.show2=!this.show2;
-                }, 400);
+                }, 0);
                 setTimeout(() => {
                     this.show3=!this.show3;
-                }, 600);
+                }, 200);
                 setTimeout(() => {
                     this.show4=!this.show4;
-                }, 800);
+                }, 400);
                 setTimeout(() => {
                     this.show5=!this.show5;
-                }, 1000);
+                }, 600);
                 setTimeout(() => {
                     this.show6=!this.show6;
-                }, 1200);
+                }, 800);
                 setTimeout(() => {
                     this.show7=!this.show7;
-                }, 1400);
-            }, 1600);
-        }
-            
-    }
-        
-    
+                }, 1000);
+                setTimeout(() => {
+                    this.show1=!this.show1;
+                }, 1200);
+            }, 1400);
+    },
 }
+    
 </script>
 
 <style>
-
 .fade-enter-active, .fade-leave-active {
-  transition: all 1s;
+  transition: all 0.6s;
 }
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+.fade-enter, .fade-leave-to {
   opacity: 0;
+  -ms-transform: scale(0);
+  -moz-transform: scale(0);
+  -webkit-transform: scale(0);
+  -o-transform:scale(0);
+  transform: scale(0);
+
 }
 
 #picture{
     width:620px;
     height:576px;
     position:relative;
-    margin-left:100px
+    margin-left:1000px
 }
 #center{
     position: absolute;
@@ -125,18 +178,7 @@ export default {
     top:255px;
     left:1px;
 }
-
-.Hexagon:after {
-	content: "";
-	position: absolute;
-	display: block;
-	top: -157px;
-	width: 0;
-	height: 0;
-	border: 100px solid transparent;
-	border-bottom: 57px solid rgb(255, 208, 0);
-}
-.Hexagon:before {
+.beforeHexagon{
 	content: "";
 	position: absolute;
 	display: block;
@@ -144,6 +186,17 @@ export default {
 	width: 0;
 	height: 0;
 	border: 100px solid transparent;
-	border-top: 57px solid rgb(255, 208, 0);
+	border-top: 57px solid;
 }
+.afterHexagon{
+	content: "";
+	position: absolute;
+	display: block;
+	top: -157px;
+	width: 0;
+	height: 0;
+	border: 100px solid transparent;
+	border-bottom: 57px solid ;
+}
+
 </style>
